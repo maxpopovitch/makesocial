9f8f4c93ae85581da4df310315e9d8249c5e0140
maxdonetsk <max.donetsk@mail.ru>
2015-07-17 01:29:40 +0300

    custom.js function changes

--- template/js/custom.js
+++ template/js/custom.js
@@ -1,19 +1,32 @@
-$(document).ready(function () {
-    function heightEqualizing() {
-        var element = $('.tm-whatwedo li');
-        // Get an array of all element heights
-        var elementHeights = element.map(function () {
-            return $(this).height();
-        }).get();
+function heightCalculation () {
+    var element = $('.tm-whatwedo li > a p');
+    var lastHeight = element.css('height');
+    function checkForChanges() {
+        if (element.css('height') != lastHeight)
+        {
+            lastHeight = element.css('height');
+        }
 
-        // Math.max takes a variable number of arguments
-        // `apply` is equivalent to passing each height as an argument
-        var maxHeight = Math.max.apply(null, elementHeights);
-
-        // Set each height to the max height
-        element.height(maxHeight);
-        console.log(maxHeight);
+        setTimeout(checkForChanges, 500);
     }
+};
 
-    $(window).resize(heightEqualizing);
-});
+var heightEqualizing = function () {
+    heightCalculation();
+    var element = $('.tm-whatwedo li');
+    // Get an array of all element heights
+    var elementHeights = element.map(function () {
+        return $(this).height();
+    }).get();
+
+    // Math.max takes a variable number of arguments
+    // `apply` is equivalent to passing each height as an argument
+    var maxHeight = Math.max.apply(null, elementHeights);
+
+    // Set each height to the max height
+    element.height(maxHeight);
+    console.log(maxHeight);
+};
+
+$(window).resize(heightEqualizing);
+$(document).ready(heightEqualizing);
