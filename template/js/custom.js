b778e4e7f139f06cc0caf47004c5d0840d19deef
maxdonetsk <max.donetsk@mail.ru>
2015-07-17 10:20:18 +0300

    height for li elements in whatwedo is done

--- template/js/custom.js
+++ template/js/custom.js
@@ -1,31 +1,14 @@
-function heightCalculation () {
-    var element = $('.tm-whatwedo li > a p');
-    var lastHeight = element.css('height');
-    function checkForChanges() {
-        if (element.css('height') != lastHeight)
-        {
-            lastHeight = element.css('height');
-        }
-
-        setTimeout(checkForChanges, 500);
-    }
-};
-
 var heightEqualizing = function () {
-    heightCalculation();
     var element = $('.tm-whatwedo li');
-    // Get an array of all element heights
-    var elementHeights = element.map(function () {
-        return $(this).height();
-    }).get();
+    var maxHeight = 0;
+    element.each(function () {
+        var height = $(this).css('height', 'auto').height();
+        if (height > maxHeight) {
+            maxHeight = height;
+        }
+    });
 
-    // Math.max takes a variable number of arguments
-    // `apply` is equivalent to passing each height as an argument
-    var maxHeight = Math.max.apply(null, elementHeights);
-
-    // Set each height to the max height
-    element.height(maxHeight);
-    console.log(maxHeight);
+    element.css('height', maxHeight);
 };
 
 $(window).resize(heightEqualizing);
