a0313e3bb1229aa0dc43de6cc6e9f12243ef4ab7
maxdonetsk <max.donetsk@mail.ru>
2015-07-18 18:20:46 +0300

    our case studies section is done

--- template/js/custom.js
+++ template/js/custom.js
@@ -1,14 +1,23 @@
 var heightEqualizing = function () {
-    var element = $('.tm-whatwedo li');
-    var maxHeight = 0;
-    element.each(function () {
+    var element1 = $('.tm-whatwedo li p');
+    var element2 = $('.tm-casestudies li span');
+    var maxHeight1 = 0;
+    var maxHeight2 = 0;
+    element1.each(function () {
         var height = $(this).css('height', 'auto').height();
-        if (height > maxHeight) {
-            maxHeight = height;
+        if (height > maxHeight1) {
+            maxHeight1 = height;
+        }
+    });
+    element2.each(function () {
+        var height = $(this).css('height', 'auto').height();
+        if (height > maxHeight2) {
+            maxHeight2 = height;
         }
     });
 
-    element.css('height', maxHeight);
+    element1.css('height', maxHeight1);
+    element2.css('height', maxHeight2);
 };
 
 $(window).resize(heightEqualizing);
