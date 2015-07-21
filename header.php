c5193d3f633d5469334949d6bfd230295f6c08f3
maxdonetsk <max.donetsk@mail.ru>
2015-07-21 11:35:46 +0300

    making php structure of index.php

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Niche Social Network Development</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,700,300,400' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="template/css/style.css">
+    </head>
+    <body>
+        <div class="tm-wrapper clearfix">
+            <nav class="navbar">
+                <div class="container tm-container-wide">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+                            <span class="tm-menu"></span>
+                        </button>
+                        <a class="navbar-brand" href="http://www.nichesocio.com/">
+                            <img src="img/logo.svg" alt="Nichesocio" />
+                        </a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="navbar">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">why us</a></li>
+                            <li><a href="#">meet us</a></li>
+                            <li><a href="#">services</a></li>
+                            <li><a href="#">our process</a></li>
+                            <li><a href="#">our work</a></li>
+                            <li><a href="#">careers</a></li>
+                            <li><a href="#">contact us</a></li>
+                            <li><a href="#">free quote</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
