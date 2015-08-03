3805d4470de31067d3709a188107bc349257db69
maxdonetsk <max.donetsk@mail.ru>
2015-08-03 17:33:14 +0300

    file uploadig form modifications

--- sendmail/quote-file.php
+++ sendmail/quote-file.php
@@ -6,7 +6,7 @@ if (!is_dir($uploaddir))
     mkdir($uploaddir, 0777);
 
 //echo $_FILES['quote-name']['size'];
-print_r($_FILES['quote-name']);
+print_r($_FILES);
 //if (move_uploaded_file($_FILES['quote-name']['tmp_name'], $uploaddir . basename($_FILES['quote-name']['name']))) {
 //    $files = realpath($uploaddir . $_FILES['quote-name']['name']);
 //}
