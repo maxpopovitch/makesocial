60ce1a3a6109bd3f23ca056d29394c237a7aef93
maxdonetsk <max.donetsk@mail.ru>
2015-08-03 13:54:56 +0300

    file uploadig form modifications

--- sendmail/quote-file.php
+++ sendmail/quote-file.php
@@ -18,7 +18,7 @@ if (isset($_GET['file'])) {
     // переместим файлы из временной директории в указанную
     foreach ($_FILES as $file) {
         if (move_uploaded_file($file['tmp_name'], $uploaddir . basename($file['name']))) {
-            echo basename($file['name']);
+            echo '123';
             $files[] = realpath($uploaddir . $file['name']);
         } else {
             $error = true;
