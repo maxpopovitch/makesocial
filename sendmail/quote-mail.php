a8fd2d66394431eb36f7ce7ec59e3a04ff58a1be
maxdonetsk <max.donetsk@mail.ru>
2015-08-04 09:31:20 +0300

    file uploadig form modifications

--- sendmail/quote-mail.php
+++ sendmail/quote-mail.php
@@ -36,7 +36,7 @@ $multipart .= chunk_split(base64_encode(iconv("utf8", "windows-1251", $message))
 // Закачиваем файл 
 $fp = fopen($filepath, "r");
 if (!$fp) {
-    print "Не удается открыть файл22";
+    print "Не удается открыть файл $filepath";
     exit();
 }
 $file = fread($fp, filesize($filepath));
