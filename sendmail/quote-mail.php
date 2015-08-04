a37800d01264671bfd74974c9354b848f34135b8
maxdonetsk <max.donetsk@mail.ru>
2015-08-04 10:08:51 +0300

    sending mail script correction

--- sendmail/quote-mail.php
+++ sendmail/quote-mail.php
@@ -12,18 +12,16 @@ $budget = $_GET['budget'];
 $file = $_GET['file'];
 $description = $_GET['description'];
 
-$message = ("$name \n $email \n $location \n $phone \n $projecttype \n $budget \n $file \n $description");
+$message = ("Name: $name <br/> Email: $email \r\n $location \n $phone \n $projecttype \n $budget \n $description");
 
 $filename = $file;
 
 $filepath = $file;
 
 $boundary = "--" . md5(uniqid(time()));
-// генерируем разделитель
 
 $mailheaders = "MIME-Version: 1.0;\r\n";
 $mailheaders .="Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
-// разделитель указывается в заголовке в параметре boundary 
 
 $mailheaders .= "From: $user_email <$user_email>\r\n";
 $mailheaders .= "Reply-To: $user_email\r\n";
@@ -33,8 +31,6 @@ $multipart .= "Content-Type: text/html; charset=windows-1251\r\n";
 $multipart .= "Content-Transfer-Encoding: base64\r\n";
 $multipart .= "\r\n";
 $multipart .= chunk_split(base64_encode(iconv("utf8", "windows-1251", $message)));
-// первая часть само сообщение
-// Закачиваем файл 
 $fp = fopen($filepath, "r");
 if (!$fp) {
     print "Не удается открыть файл $filepath";
@@ -42,8 +38,6 @@ if (!$fp) {
 }
 $file = fread($fp, filesize($filepath));
 fclose($fp);
-// чтение файла
-
 
 $message_part = "\r\n--$boundary\r\n";
 $message_part .= "Content-Type: application/octet-stream; name=\"$filename\"\r\n";
@@ -52,7 +46,6 @@ $message_part .= "Content-Disposition: attachment; filename=\"$filename\"\r\n";
 $message_part .= "\r\n";
 $message_part .= chunk_split(base64_encode($file));
 $message_part .= "\r\n--$boundary--\r\n";
-// второй частью прикрепляем файл, можно прикрепить два и более файла
 
 $multipart .= $message_part;
 
@@ -62,10 +55,7 @@ if (mail($mailto, $subject, $multipart, $mailheaders)) {
     echo "error";
 };
 
-// отправляем письмо 
-//удаляем файлы через 60 сек.
-//if (time_nanosleep(5, 0)) {
-//    unlink($filepath);
-//}
-// удаление файла
+if (time_nanosleep(5, 0)) {
+    unlink($filepath);
+}
 ?>
