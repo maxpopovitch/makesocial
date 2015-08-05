52dfca3595cf1b826ab1f4be6c86ee86c6153c86
maxdonetsk <max.donetsk@mail.ru>
2015-08-05 12:03:32 +0300

    changing path to demo.nowa.center/pytex

--- includes/404-content.php
+++ includes/404-content.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 text-center">
-                    <img class="img-responsive" src="../img/image-404.svg" alt="404" />
+                    <img class="img-responsive" src="http://demo.nowa.center/pytex/img/image-404.svg" alt="404" />
                     <h2><strong>Oops, it looks like this page does not exist.</strong></h2>
                     <p class="h5"><strong>The link you followed may be broken, forbidden, or the page may have been removed.</strong></p>
                     <a class="h4" href="http://pytex.net/"><strong>Take me home</strong></a>
