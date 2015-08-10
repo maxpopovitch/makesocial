99c086f3782acb1b1d5bb524226c48beed4dcaf2
maxdonetsk <max.donetsk@mail.ru>
2015-08-10 18:10:03 +0300

    markup preparations for procurious

--- includes/procurious-content.php
+++ includes/procurious-content.php
@@ -7,17 +7,54 @@
 </header>
 </div>
 <main>
-    <div class="tm-bg-01">
-        <div class="container tm-sect-01">
+    <div class="tm-bg-01 tm-overflow-hidden tm-procurious-brief-bg">
+        <div class="container">
             <div class="row">
-                <div class="col-md-5">
+                <div class="col-md-5 tm-brief-content">
                     <h2 class="text-center"><strong>Brief</strong></h2>
                     <p class="tm-default-p">Procurious approached EtonDigital to create a global online network and knowledge hub for young procurement professionals with the aim of enticing them to connect, engage and learn, oversee professional awards and networking events, and offer an effective platform to sell and deliver online trainings.</p>
                     <p class="tm-default-p">The core feature of the website is the networking function, which operates in the same way as a LinkedIn style website. Users must be able to message each other and connect with one another, share articles they’ve read, have a profile with contact details and CV, and join groups and other discussion sections.</p>
                 </div>
-                <div class="col-md-7 visible-md visible-lg">
-                    <img class="tm-screenshot img-responsive" src="img/our-work/portfolio/procurious/procurious-screen-big-01.jpg" alt="Procuriuos" />
+                <div class="col-md-7 tm-sect-01 visible-md visible-lg">
+                    <div class="tm-brief-screenshot">
+                        <img src="img/our-work/portfolio/procurious/procurious-screen-big-01.jpg" alt="Procuriuos" />
+                    </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div class=" tm-sect-01 tm-procurious-discovery-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2 class="text-center"><strong>Discovery</strong></h2>
+                    <p class="tm-default-p">After a 2 day workshop with the clients during which we discussed the individual requirements, user journeys and core components of the business, we created the initial Product Backlog that captured user stories needed for the development of MVP.</p>
+                </div>
+            </div>
+        </div>
+        <div class="tm-container-wide">
+            <div class="text-center">
+                <img class="img-responsive visible-md-inline-block visible-lg-inline-block" src="img/our-work/portfolio/procurious/procurious-screen-big-02.png" alt="Procuriuos" />
+            </div>
+        </div>
+    </div>
+    <div class=" tm-sect-01 tm-bg-01">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h3 class="text-uppercase"><strong>Sitemap</strong></h3>
+                </div>
+                <div class="col-md-6">
+                    <p class="tm-default-p">Procurious is a complex, powerful platform, but we knew from the start that maintaining a level of simplicity around navigation and functionality would be key to keeping users engaged and for them to easily find their way around from the moment they first landed on the website. Procurious consists of the main Procurious portal available to all registered members and different private member areas which are called Roundtables.</p>
+                </div>
+                <div class="col-md-6">
+                    <p class="tm-default-p">The biggest challenge was to let users easily switch the portal they are on and still keep the same flow and structure, but with completely different branding for each Roundtable. The userflow maps out how various screens relate to each other and how users navigate between them.</p>
+                </div>
+            </div>
+        </div>
+        <div class="tm-container-wide">
+            <div class="text-center">
+                <img class="img-responsive visible-md-inline-block visible-lg-inline-block" src="img/our-work/portfolio/procurious/procurious-screen-big-03.png" alt="Procuriuos" />
             </div>
         </div>
     </div>
