eebbc7529a0de4568563d84e0128d96bf800ea26
maxdonetsk <max.donetsk@mail.ru>
2016-01-07 23:19:52 +0200

    navbar icons changed to svg

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,14 @@
                 <div class="container tm-container-wide">
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
-                            <span class="tm-menu"></span>
+                            <svg class="tm-menu-collapsed" preserveAspectRatio="xMinYMin meet" viewBox="0 0 20 20">
+                                <path d="M17.5 6h-15c-0.276 0-0.5-0.224-0.5-0.5s0.224-0.5 0.5-0.5h15c0.276 0 0.5 0.224 0.5 0.5s-0.224 0.5-0.5 0.5z"></path>
+                                <path d="M17.5 11h-15c-0.276 0-0.5-0.224-0.5-0.5s0.224-0.5 0.5-0.5h15c0.276 0 0.5 0.224 0.5 0.5s-0.224 0.5-0.5 0.5z"></path>
+                                <path d="M17.5 16h-15c-0.276 0-0.5-0.224-0.5-0.5s0.224-0.5 0.5-0.5h15c0.276 0 0.5 0.224 0.5 0.5s-0.224 0.5-0.5 0.5z"></path>
+                            </svg>
+                            <svg class="tm-menu-not-collapsed" preserveAspectRatio="xMinYMin meet" viewBox="0 0 20 20">
+                                <path d="M10.707 10.5l5.646-5.646c0.195-0.195 0.195-0.512 0-0.707s-0.512-0.195-0.707 0l-5.646 5.646-5.646-5.646c-0.195-0.195-0.512-0.195-0.707 0s-0.195 0.512 0 0.707l5.646 5.646-5.646 5.646c-0.195 0.195-0.195 0.512 0 0.707 0.098 0.098 0.226 0.146 0.354 0.146s0.256-0.049 0.354-0.146l5.646-5.646 5.646 5.646c0.098 0.098 0.226 0.146 0.354 0.146s0.256-0.049 0.354-0.146c0.195-0.195 0.195-0.512 0-0.707l-5.646-5.646z"></path>
+                            </svg>
                         </button>
                         <a class="navbar-brand" href="/">
                             <img src="img/logo.png" alt="MakeSocial" />
