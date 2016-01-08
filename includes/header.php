0ef5ff909cd0c0b9d8b0bdf122dc3b8250540855
maxdonetsk <max.donetsk@mail.ru>
2016-01-08 13:36:30 +0200

    small htaccess improvement

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>MakeSocial | Social Network Development Team</title>
+        <title>MakeSocial | Social Networking Development Team</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
